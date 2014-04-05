56d4b267ee5e5a27d5e7c8d11d7d733a77d54b8a
Krishan Taylor <krishan.taylor@gmail.com>
2014-04-05 14:16:44 -0400

    add dummy test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Sudoku', function () {
+  describe('dummy', function () {
+    it('should be true', function () {
+      assert(true);
+    });
+  });
+});
